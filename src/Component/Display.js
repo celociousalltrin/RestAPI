52f35604101d3d93faf8637f5a10cbd392b9d10d
Celocious <cfranklin@inniveinc.com>
2022-10-25 07:53:25 +0530

    RestAPI with CRUD completed

--- src/Component/Display.js
+++ src/Component/Display.js
@@ -1,55 +1,52 @@
 import React from 'react';
-import { useEffect,useState } from 'react';
-import CustomAPI from "../Component/baseAPI";
+import {useState, useEffect} from "react";
+import CustomAPI from "./baseAPI";
 import View from "./View";
 
 const Display = () => {
-    const[user,setUser] = useState([])
-    const [username,setUsername] = useState("")
-    const [name,setName] = useState("")
-    const[email,setEmail] = useState("")
+const [details, setDetails] = useState([])
+const [name,setName] = useState("")
+const [username, setUsername] = useState("")
+const [email,setEmail] = useState("")
 
-    useEffect(()=>{
-     CustomAPI.get("/Profile")
-     .then((res)=>setUser(res.data))
-     .catch((err)=>console.log(err))
-    },[])
+useEffect(()=>{
+CustomAPI.get("/Profile")
+.then((res)=>setDetails(res.data))
+.catch((err)=>console.log(err))
+},[])
 
-    const Adduser =(()=>{
-        const OBJ = {
-            id:user[user.length-1].id+1,
-            username:username,
-            name:name,
-            email:email,
-        }
-        CustomAPI.post("/Profile",OBJ)
-        .then((res)=>console.log(res.data))
-        .catch((err)=>console.log(err))
-    })
 const renderIteration = (()=>{
-    return user.map((item)=><View key={item.id} {...item} />)
+    return details.map((item)=><View key={item.id} {...item}  /> )
+})
+
+const AddUser = (()=>{
+    const OBJ = {
+        id:Math.random,
+        name:name,
+        username:username,
+        email:email,
+    }
+
+    CustomAPI.post("/Profile", OBJ)
+    .then((res)=>console.log(res))
+    .catch((err)=>console.log(err))
 })
   return (
     <div>
-        <form>
-        <input id="username" name="username" 
-        type="text" placeholder="username..." 
-        onChange={(e)=>setUsername(e.target.value)}/>
-
-         <input id="name" name="name" 
-        type="text" placeholder="name..." 
-        onChange={(e)=>setName(e.target.value)}/>
-
-         <input id="email" name="email" 
-        type="email" placeholder="email..." 
-        onChange={(e)=>setEmail(e.target.value)}/>
         
-        <button onClick={Adduser}>Add</button>
+        <form>
+            <input type="text" name="name" id="name"
+            placeholder='name..' onChange={(e)=>setName(e.target.value)}/>
+            <input type="text" name="username" id="username"
+            placeholder='username..' onChange={(e)=>setUsername(e.target.value)}/>
+            <input type="email" name="email" id="email"
+            placeholder='email..' onChange={(e)=>setEmail(e.target.value)}/>
+        
+        <button onClick={AddUser}>Add</button>
         </form>
-
         {renderIteration()}
     </div>
   )
 }
 
-export default Display;
+export default Display
--- src/Component/View.js
+++ src/Component/View.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CustomAPI from "./baseAPI";
 
 const View = ({id,name,username,email}) => {
-const Deleteuser = (()=>{
-    CustomAPI.delete(`/Profile/${id}`)
-    .then((res)=>console.log(res))
-    .catch((err)=>console.log(err))
+  const [upDetail, setUpDetail] = useState({
+    Name:"",
+    UserName:"",
+    Mail:"",
+  })  
+
+  const handleChange = (e)=>{
+    setUpDetail((prev)=>({...prev, [e.target.name]:e.target.value}))
+  }
+
+  const UpdateDetails = ()=>{
+    const OBJ = {
+      name:upDetail.Name,
+      username:upDetail.UserName,
+      email:upDetail.Mail,
+    }
+
+    CustomAPI.put(`/Profile/${id}`, OBJ)
+    .then((res)=>console.log(res.data))
+  }
+
+    const DeleteUser = (()=>{
+        CustomAPI.delete(`/Profile/${id}`)
+   .then((res)=>console.log(res))
+   .catch((err)=>console.log(err))
     })
 
   return (
     <div>
-        <h1>{username}</h1>
-        <h2>{name}</h2>
+        <h2>{username}</h2>
+        <h3>{name}</h3>
         <h3>{email}</h3>
         <form>
-            <button onClick={Deleteuser}>Delete</button>
+          <input type="text" placeholder="name..." name="Name" onChange={handleChange}/>
+          <input type="text" placeholder="username..." name="UserName" onChange={handleChange}/>
+          <input type="email" placeholder="mail..." name="Mail" onChange={handleChange}/>
+          <button onClick={UpdateDetails}>Update</button>
+        <button onClick={DeleteUser}>Delete</button>
         </form>
+      
     </div>
   )
 }
